507d340758143ef74138c3b813b7d19c905c64f2
Ozcar Cayetano <ozcar@Ozcars-MacBook-Pro.local>
2024-10-28 23:02:21 -0700

    brackets should be able to be created but it doesnt work locally

--- src/api/brackets.py
+++ src/api/brackets.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
+import datetime
 
 import sqlalchemy
 from src import database as db
@@ -10,3 +11,32 @@ router = APIRouter(
     tags=["brackets"],
     dependencies=[Depends(auth.get_api_key)],
 )
+
+class Bracket(BaseModel):
+    name: str
+    date_time: datetime.datetime
+    game: str
+    capacity: int
+    cost:float
+
+@router.post("/brackets")
+def create_bracket(new_bracket: Bracket):
+    bracket_dict = {
+        "name": new_bracket.name,
+        "date_time": new_bracket.date_time,
+        "game": new_bracket.game,
+        "capacity": new_bracket.capacity,
+        "cost": new_bracket.cost,
+    }
+
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+                    INSERT INTO brackets 
+                    (name, date_time, game, capacity, cost) 
+                    VALUES (:name, :date_time, :game, :capacity, :cost) 
+                    RETURNING id """), bracket_dict).scalar_one_or_none()
+    if result is None:
+        print("Bracket could not be created")
+        return {"bracket_id": None}
+    else:
+        return {"bracket_id": result}
