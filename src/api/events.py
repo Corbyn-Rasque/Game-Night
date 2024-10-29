c999c62537f40a8a5d4704b788d2d929b5775c1c
Ozcar Cayetano <ozcar@Ozcars-MacBook-Pro.local>
2024-10-28 23:31:00 -0700

    did the basemodel stuff

--- src/api/events.py
+++ src/api/events.py
@@ -14,23 +14,24 @@ router = APIRouter(
     tags=["events"],
     dependencies=[Depends(auth.get_api_key)],
 )
-@router.post("/events")
-def create_events(event_name: str, time: datetime.datetime, type: str, max_attendees: int, location: str):
 
-    event_dict = {
-        "event_name": event_name,
-        "time": time,
-        "type": type,
-        "max_att": max_attendees,
-        "location": location
-    }
+
+class Event(BaseModel):
+    event_name: str
+    time: datetime.datetime
+    type: str
+    max_attendees: int
+    location: str
+
+@router.post("/events")
+def create_events(new_event: Event):
 
     with db.engine.begin() as connection:
         event_id = connection.execute(sqlalchemy.text("""
                         INSERT INTO event 
                         (name, date_time, type, max_attendees, locations) 
-                        VALUES (:event_name, :time, :type, :max_att, :location) 
-                        RETURNING id"""),event_dict).scalar_one_or_none()
+                        VALUES (:event_name, :time, :type, :max_attendees, :location) 
+                        RETURNING id"""),dict(new_event)).scalar_one_or_none()
 
         if event_id is None:
             # try getting new event info again
