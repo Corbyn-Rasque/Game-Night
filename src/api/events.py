e1148592ec3338c41b396ea21d65de5e0619b03c
Corbyn-Rasque <corbyn@rasque.dev>
2024-11-05 03:22:41 -0800

    Updated events to have date search functionality.

--- src/api/events.py
+++ src/api/events.py
@@ -5,13 +5,16 @@ import datetime
 from sqlalchemy import text
 from src import database as db
 
-# MAKE SURE TO USE UTC TIME!!!
-
 router = APIRouter(
     prefix="/events",
     tags=["events"],
     dependencies=[Depends(auth.get_api_key)],
 )
+
+# MAKE SURE TO USE UTC TIME!!!
+
+# Test date functionality with URL. Verify format that it's being received in works,
+# and that query parameters are also working properly, as well.
 
 class Event(BaseModel):
     event_name: str
@@ -24,39 +27,52 @@ class Event(BaseModel):
 
 # Create new event
 @router.post("/")
-def create_event(new_event: Event):
+def create_event(event: Event):
     create_event = text('''INSERT INTO events (name, date_time, active, type, max_attendees, location) 
                            VALUES (:event_name, :time, :active, :type, :max_attendees, :location)
                            RETURNING id''')
 
     with db.engine.begin() as connection:
-        event_id = connection.execute(create_event, dict(new_event)).scalar_one_or_none()
+        event_id = connection.execute(create_event, dict(event)).scalar_one_or_none()
 
     return event_id if event_id else {}
 
 
 # Get event details by event id
-@router.get("/{event_id}")
-def get_event(event_id: int):
+@router.get("/")
+def get_event_by_id(event: int):
     event_query = text('''SELECT name, type, active, location, max_attendees, date_time
-                        FROM events
-                        WHERE id = :event_id''')
+                          FROM events
+                          WHERE id = :event_id''')
 
     with db.engine.begin() as connection:
-        result = connection.execute(event_query, {"event_id": event_id}).mappings().all()
+        result = connection.execute(event_query, {"event_id": event}).mappings().all()
     
-    return result
+    return result if result else {}
+
+
+# Get event details by date
+@router.get("/")
+def get_event_by_date(date: datetime.datetime = datetime.datetime.today()):
+    event_query = text('''SELECT name, type, active, location, max_attendees, date_time
+                          FROM events
+                          WHERE (date_time - date(':year.:month.:day')) < '1 Day' ''')
+    
+    with db.engine.begin() as connection:
+        result = connection.execute(event_query, {"year": date.year, "month": date.month, "day": date.day}).mappings().all()
+    
+    return result if result else {}
 
 
 # Cancel an event
-@router.post("/{event_id}")
-def cancel_event(event_id: int):
+@router.delete("/")
+def cancel_event(event: int):
     cancel_event = text('''UPDATE events
                            SET cancelled = TRUE
                            WHERE id = :event_id''')
     
     with db.engine.begin() as connection:
-        connection.execute(cancel_event, {"event_id": event_id})
+        connection.execute(cancel_event, {"event_id": event})
 
     return "OK"
 
