041f24d501753efd91837be3ce4904bd53474143
Alan Castillo <acast125@calpoly.edu>
2024-11-05 00:51:28 -0800

    event get by event_id

--- src/api/events.py
+++ src/api/events.py
@@ -13,26 +13,39 @@ router = APIRouter(
     dependencies=[Depends(auth.get_api_key)],
 )
 
-
 class Event(BaseModel):
     event_name: str
-    time: datetime.datetime
+    time: datetime.datetime  # Ensure this is UTC
     type: str
+    active: str
     max_attendees: int
     location: str
 
 @router.post("/")
 def create_events(new_event: Event):
-
+    
     with db.engine.begin() as connection:
         event_id = connection.execute(sqlalchemy.text("""
-                        INSERT INTO events 
-                        (name, date_time, type, max_attendees, locations) 
-                        VALUES (:event_name, :time, :type, :max_attendees, :location) 
-                        RETURNING id"""),dict(new_event)).scalar_one_or_none()
+        INSERT INTO events 
+        (name, date_time, active, type, max_attendees, location) 
+        VALUES (:event_name, :time, :active, :type, :max_attendees, :location) 
+        RETURNING id
+        """), new_event.dict()).scalar_one_or_none()
 
-        if event_id is None:
-            # try getting new event info again
-            return {"event_id": None}
-        else:
-            return {"event_id": event_id}
+    if event_id is None:
+        return {"event_id": None}
+    else:
+        return {"event_id": event_id}
+
+@router.get("/{event_id}")
+def get_event(event_id: int):
+
+    with db.engine.begin() as connection:
+        event_query = sqlalchemy.text("""
+            SELECT 
+            name, type, active, location, max_attendees, date_time
+            FROM events
+            WHERE id = :id """)
+        result = connection.execute(event_query, {"id": event_id}).mappings().all()
+    
+    return result
