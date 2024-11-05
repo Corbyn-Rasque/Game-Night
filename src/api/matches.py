25813f363cbab6e6800aa24a746bad5ee2fc2ffd
OzcarC <olopez2015.ol@gmail.com>
2024-11-05 12:31:29 -0800

    inserted matches now

--- src/api/matches.py
+++ src/api/matches.py
@@ -21,29 +21,27 @@ def get_matches(bracket_id: int):
 
     return result if result else []
 
+
 class Match(BaseModel):
     bracket_id: int
-    best_of: int
-    round:int
+    match_amount: int
+
 @router.post("/")
-def create_match(new_match:Match,matches_amount: int):
+def create_match(new_match:Match):
     match_arr = []
-    for i in range(matches_amount):
+    for i in range(new_match.match_amount):
         match_arr.append(
             {
                 "bracket_id": new_match.bracket_id,
-                "best_of": new_match.best_of,
-                "round": new_match.round,
             }
         )
     with db.engine.begin() as conn:
-        result = conn.execute(sqlalchemy.text("""
-                    INSERT INTO matches (bracket_id,best_of,round) 
-                    VALUES (:bracket_id,:best_of,:round) 
-                    RETURNING id"""),match_arr).fetchall()
+        conn.execute(sqlalchemy.text("""
+                    INSERT INTO matches (bracket_id) 
+                    VALUES (:bracket_id)"""),match_arr)
 
-    return result if result else []
+    return "OK"
 
 
-test_match = Match(bracket_id = 4, best_of = 3, round = 1)
-print(create_match(test_match,8))
+test_match = Match(bracket_id = 4, match_amount = 8)
+print(create_match(test_match))
