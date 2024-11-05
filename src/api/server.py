2076b4356039808a5d4cf5ce7146ba2c1b59cc56
Corbyn-Rasque <corbyn@rasque.dev>
2024-11-05 13:42:57 -0800

    Updated server.py to include new endpoints & fixed typing/naming.

--- src/api/server.py
+++ src/api/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, exceptions
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
-from src.api import events, users, brackets, games
+from src.api import brackets, events, games, items, matches, users
 import json
 import logging
 import sys
@@ -32,10 +32,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(events.router)
-app.include_router(users.router)
 app.include_router(brackets.router)
+app.include_router(events.router)
 app.include_router(games.router)
+app.include_router(items.router)
+app.include_router(matches.router)
+app.include_router(users.router)
 
 @app.exception_handler(exceptions.RequestValidationError)
 @app.exception_handler(ValidationError)
