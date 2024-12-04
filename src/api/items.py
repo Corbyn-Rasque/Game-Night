9371010a73fa42e6eaed8bce03f9d48926a2026d
Alan Castillo <acast125@calpoly.edu>
2024-12-04 02:21:30 -0800

    CHANGED ENPOINTS :o

--- src/api/items.py
+++ src/api/items.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
+from typing import Optional
 from src.api import auth
 from sqlalchemy import text
 from src import database as db
@@ -13,124 +14,199 @@ router = APIRouter(
 # In the current state, contributions cannot be made unless already requested (foreign key violation otherwise).
 # Consider how we might want to handle this (allow / disallow based on event & table logic?)
 
-# Should deletion of users items happen separately in the Users endpoint? URL setup doesn't lend well to deleting
-# by items here.
 
 class Item(BaseModel):
     name: str
     type: str
     quantity: int
-    payment: int
+    cost: int
 
 # Insert item requests for an event, or update if it's been added
-@router.post("/{event_id}/requests")
-def request_item(event_id: int, item: Item):
+@router.post("/{event_id}/request")
+def request_new_item(event_id: int, item: Item):
     request = text('''INSERT INTO event_items (event_id, name, type, requested, cost)
-                      VALUES (:event_id, :name, :type, :quantity, :payment)
-                      ON CONFLICT (event_id, name)
-                      DO UPDATE SET (type, requested, cost) = (EXCLUDED.type, EXCLUDED.requested, EXCLUDED.cost)''')
+                      VALUES (:event_id, :name, :type, :quantity, :cost)''')
     
-    with db.engine.begin() as connection:
-        connection.execute(request, dict(item) | {'event_id': event_id})
-
-    return "OK"
-
-
-# Insert an individual contribution, or update if it's been added
-@router.post("/{event_id}/contributions/user/{username}")
-def contribute_item(event_id: int, username: str, item: Item):
-    check_item_exists = text('''SELECT TRUE
-                                FROM event_items
-                                WHERE (event_id, name) IN ((:event_id, :name))''')
-
-    contribute = text('''INSERT INTO items_ledger (event_id, username, item_name, quantity, payment)
-                         VALUES (:event_id, :username, :name, :quantity, :payment)
-                         ON CONFLICT (event_id, username, item_name)
-                         DO UPDATE SET (quantity, payment) = (EXCLUDED.quantity, EXCLUDED.payment)''')
-
-    with db.engine.begin() as connection:
-        if not connection.execute(check_item_exists, dict(item) | {'event_id': event_id, 'username': username}).scalar_one_or_none():
-            request_item(event_id, item)
-
-        connection.execute(contribute, dict(item) | {'event_id': event_id, 'username': username})
-
+    try:
+        with db.engine.begin() as connection:
+            connection.execute(request, dict(item) | {'event_id': event_id})
+    except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occurred while updating the contribution: {str(e)}")
+    
     return "OK"
 
 
 # Get contributions overall, grouped by item
-@router.get("/{event_id}/contributions/")
-def contributions(event_id: int):
-    get_contributions = text('''SELECT item_name, SUM(quantity)::INTEGER AS total, SUM(payment)::INTEGER AS contribution
-                                FROM items_ledger
-                                WHERE (event_id, deleted) IN ((:event_id, FALSE))
-                                GROUP BY item_name''')
+@router.get("/{event_id}/request")
+def get_current_contributions(event_id: int):
+
+    get_contributions = text('''
+                            with item_quantity as (
+                            SELECT event_id, item_name, 
+                            SUM(quantity)::int AS total, 
+                            SUM(payment)::int AS payments
+                            FROM item_contributions
+                            GROUP BY event_id, item_name)
+
+                            SELECT name, type, requested, COALESCE(total,0) as Total, 
+                            COALESCE(payments,0) as Payments FROM event_items
+                            LEFT JOIN item_quantity ON item_quantity.event_id = event_items.event_id 
+                            AND item_quantity.item_name= event_items.name
+                            WHERE (event_items.event_id, deleted) IN ((:event_id, FALSE))''')
     
-    with db.engine.begin() as connection:
-        contributions = connection.execute(get_contributions, {'event_id': event_id}).mappings().all()
+    try:
+        with db.engine.begin() as connection:
+            contributions = connection.execute(get_contributions, {'event_id': event_id}).mappings().all()
+    except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occurred while updating the contribution: {str(e)}")
 
     return contributions
+
+class delete_items(BaseModel):
+    name: str
+
+@router.delete("/{event_id}/request")
+def remove_item_request(event_id: int, to_delete: list[delete_items]):
+
+    items_to_delete = []
+    for item in to_delete:
+        items_to_delete.append(
+            {'name': item.name,
+            'event_id': event_id})
+
+    remove_contributions = text('''
+                                DELETE FROM item_contributions
+                                WHERE event_id = :event_id AND
+                                item_name = :name; 
+
+                                DELETE FROM event_items
+                                WHERE event_id = :event_id AND
+                                name = :name;  ''')
+    
+    try:
+        with db.engine.begin() as connection:
+            connection.execute(remove_contributions, items_to_delete)
+    except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occurred while updating the contribution: {str(e)}")
+    
+    print("Items Deleted:")
+    print(items_to_delete)
+
+    return "OK"
 
 
 # Get contributions from a single user
-@router.get("/{event_id}/contributions/{username}")
-def user_contribution(event_id: int, username: str):
-    get_contributions = text('''SELECT item_name, SUM(quantity)::INTEGER AS total, SUM(payment)::INTEGER AS contribution
-                                FROM items_ledger
+@router.get("/{event_id}/{username}")
+def user_contributions(event_id: int, username: str):
+    get_contributions = text('''SELECT item_name, SUM(quantity) AS total, SUM(payment) AS payment
+                                FROM item_contributions
                                 WHERE (event_id, username, deleted) IN ((:event_id, :username, FALSE))
                                 GROUP BY item_name''')
+    try:
+        with db.engine.begin() as connection:
+            contributions = connection.execute(get_contributions, {'event_id': event_id, 'username': username}).mappings().all()
+    except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occurred while updating the contribution: {str(e)}")
     
-    with db.engine.begin() as connection:
-        contributions = connection.execute(get_contributions, {'event_id': event_id, 'username': username}).mappings().all()
-
     return contributions
 
 
-@router.patch("/{event_id}/requests/{item_name}")
-def remove_request(event_id: int, item_name: str):
-    remove_request = text('''UPDATE event_items
-                             SET deleted = TRUE
-                             WHERE (event_id, name) IN ((:event_id, :item_name))''')
-    
+class contribution(BaseModel):
+    item_name: str
+    quantity: int
+
+# Allows users to make a contribution 
+@router.post("/{event_id}/{username}")
+def contribute_item(event_id: int, username: str, item: contribution):
+
     with db.engine.begin() as connection:
-        connection.execute(remove_request, {'event_id': event_id, 'item_name': item_name})
-    
+
+        check = text('''SELECT TRUE as bool FROM event_items 
+                        WHERE name = :name AND event_id = :id ''')
+        check_params = {"name": item.item_name, "id" : event_id}
+        requested = connection.execute(check, check_params).fetchone()
+
+        if not requested:
+            raise HTTPException(status_code=404, detail=f"{item.item_name} is not currently requested for this event")
+        
+        contribute = text('''WITH item_cost AS (
+                    SELECT cost FROM event_items
+                    WHERE event_id = 40 AND name = :item_name
+                    )
+                    INSERT INTO item_contributions (event_id, username, item_name, quantity, payment)
+                    SELECT :event_id, :username, :item_name, :quantity, cost * :quantity
+                    FROM item_cost
+                    ON CONFLICT (event_id, username, item_name) DO NOTHING;''')        
+        
+        try:
+            connection.execute(contribute, dict(item) | {'event_id': event_id, 'username': username})
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occurred while updating the contribution: {str(e)}")
+        
     return "OK"
 
 
-# Delete single item contribution by an individual
-@router.patch("/{event_id}/contributions/{username}/{item_name}")
-def remove_user_contributions(event_id: int, username: str, item_name: str):
-    remove_contributions = text('''UPDATE items_ledger
-                                   SET deleted = TRUE
-                                   WHERE (event_id, username, item_name) IN ((:event_id, :username, :item_name))''')
-    
-    with db.engine.begin() as connection:
-        connection.execute(remove_contributions, {'event_id': event_id, 'username': username, 'item_name': item_name})
 
+class Updated_Item(BaseModel):
+    item_name: str
+    new_quantity: int
+
+@router.patch("/{event_id}/{username}")
+def update_item_contribuition(event_id: int, username: str, item: Updated_Item):
+
+    with db.engine.begin() as connection:
+        
+        #This can be deleted if it causes concurrency issues
+        check = text('''SELECT TRUE as bool FROM item_contributions 
+                        WHERE username = :username AND item_name = :name 
+                        AND event_id = :id ''')
+        
+        check_params = {"username": username, "name" : item.item_name, "id": event_id}
+        request = connection.execute(check, check_params).fetchone()
+
+        if not request:
+            raise HTTPException(status_code=404, detail="contribuition not found")
+        
+        update = text('''UPDATE item_contributions
+            SET 
+                quantity = new_info.quantity,
+                payment = new_info.payment
+            FROM (
+                SELECT 
+                    :quantity as quantity,
+                    cost * :quantity as payment
+                FROM event_items
+                WHERE event_items.event_id = :id AND event_items.name = :name
+            ) as new_info
+            WHERE item_contributions.event_id = :id AND item_contributions.username = :username
+            ''')
+                      
+        update_params = {"username": username, "name" : item.item_name, "id": event_id, "quantity": item.new_quantity}
+        try:
+            connection.execute(update, update_params)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occurred while updating the contribution: {str(e)}")
+            
     return "OK"
 
 
-# Delete all contributions by an individual
-@router.patch("/{event_id}/contributions/{username}")
-def remove_user_contributions(event_id: int, username: str):
-    remove_contributions = text('''UPDATE items_ledger
-                                   SET deleted = TRUE
-                                   WHERE (event_id, username) IN ((:event_id, :username))''')
+@router.delete("/{event_id}/{username}")
+def remove_user_contributions(event_id: int, username: str, to_delete: list[delete_items]):
+    items_to_delete = []
+    for item in to_delete:
+        items_to_delete.append(
+            {'name': item.name,
+            'event_id': event_id,
+            'username': username})
+        
+    remove_contributions = text('''
+                                DELETE FROM item_contributions
+                                WHERE (event_id, username, item_name) IN ((:event_id, :username, :name))
+                                ''')
+    try:
+        with db.engine.begin() as connection:
+            connection.execute(remove_contributions, items_to_delete)
+    except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occurred while updating the contribution: {str(e)}")
     
-    with db.engine.begin() as connection:
-        connection.execute(remove_contributions, {'event_id': event_id, 'username': username})
-
-    return "OK"
-
-
-# Delete all event contributions
-@router.patch("/{event_id}/contributions")
-def remove_all_event_contributions(event_id: int):
-    remove_contributions = text('''UPDATE items_ledger
-                                   SET deleted = TRUE
-                                   WHERE event_id = :event_id''')
-    
-    with db.engine.begin() as connection:
-        connection.execute(remove_contributions, {'event_id': event_id})
-
     return "OK"
